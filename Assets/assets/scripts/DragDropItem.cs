0c7f23e50960c451774d62c32e695e3e8a398e78
Vasily Tarasevich <taravask@gmail.com>
2021-02-22 11:41:40 +0300

    Initial commit
    Project in state in which it was left by previous developer

---
+++ Assets/assets/scripts/DragDropItem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    public Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    public Vector2 vector = new Vector2();
+    public bool isDropped = false;
+    public bool isHold = false;
+    
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+       
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        StopCoroutine(eventData.pointerDrag.GetComponent<SneakerScript>().route);
+        canvasGroup.alpha = 0.4F;
+        canvasGroup.blocksRaycasts = false;
+        vector = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+        canvasGroup.alpha = 1F;
+        canvasGroup.blocksRaycasts = true;
+        if (!isDropped)
+        {
+            transform.position = vector;
+            if(gameObject.GetComponent<SneakerScript>().route_index == 0)
+            {
+                gameObject.GetComponent<SneakerScript>().route = StartCoroutine(Movement.instance.MainRoute(gameObject, gameObject.GetComponent<SneakerScript>().currentPoint));
+            }
+            if(gameObject.GetComponent<SneakerScript>().route_index == 1)
+            {
+                gameObject.GetComponent<SneakerScript>().route = StartCoroutine(Movement.instance.WashRoute(gameObject, gameObject.GetComponent<SneakerScript>().currentPoint));
+            }
+            if(gameObject.GetComponent<SneakerScript>().route_index == 2)
+            {
+                gameObject.GetComponent<SneakerScript>().route = StartCoroutine(Movement.instance.ShnurRoute(gameObject, gameObject.GetComponent<SneakerScript>().currentPoint));
+            }
+        }
+        
+       
+    }
+
+
+    public void OnDrop(PointerEventData eventData)
+    {
+       
+    }
+}
